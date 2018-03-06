cbe07884e239004f530c0f4016af2f55602bc637
Justus Brown <justusbrown@mail.weber.edu>
2018-03-06 15:03:57 -0700

    Basic menu functionality

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // difficulty consts
+	const int BEGINNER = 0;
+	const int INTERMEDIATE = 1;
+
+    public void StartBeginnerGame()
+    {
+        // load first beginner scene
+    }
+
+    public void StartIntermediateGame()
+    {
+        // load first intermediate scene
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public Animator difficultyPanel;
+    public Animator profilePanel;
+
+    bool animateDiff = false;
+    bool animateProf = false;
+
+    // bool profileSelected = false;
+
+    /// <summary>
+    /// Will animate and show the Difficulty panel
+    /// </summary>
+    public void showDifficultyPanel()
+    {
+            // add code for if a profile is not selected
+            animateDiff = !animateDiff;
+            difficultyPanel.SetBool("AnimatePanel", animateDiff);
+    }
+
+    public void showProfilePanel()
+    {
+        animateProf = !animateProf;
+        profilePanel.SetBool("AnimatePanel", animateProf);
+    }
+
+}
