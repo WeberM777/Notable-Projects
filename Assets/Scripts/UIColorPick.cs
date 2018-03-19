f8eabb5da86d2724138f62c612b8e9e826425bc0
Dax Kelson <dkelson@gurulabs.com>
2018-03-19 02:23:36 -0600

    Add "what is your favorite color" mini activity

---
+++ Assets/Scripts/UIColorPick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIColorPick : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Animator GreenColorAnim;
+    public Animator BlueColorAnim;
+    public Animator RedColorAnim;
+    public Animator YellowColorAnim;
+
+    public void RunGreenAnim()
+    {
+        LoadMenuScene(1);
+        GreenColorAnim.SetTrigger("Active");
+    }
+    public void RunBlueAnim()
+    {
+        LoadMenuScene(1);
+        BlueColorAnim.SetTrigger("Active");
+    }
+    public void RunRedAnim()
+    {
+        LoadMenuScene(1);
+        RedColorAnim.SetTrigger("Active");
+    }
+    public void RunYellowAnim()
+    {
+        LoadMenuScene(1);
+        YellowColorAnim.SetTrigger("Active");
+    }
+    public void LoadMenuScene(float delay)
+    {
+        StartCoroutine(LoadMenuSceneDelay(delay));
+    }
+    private IEnumerator LoadMenuSceneDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Menu");
+    }
+}
