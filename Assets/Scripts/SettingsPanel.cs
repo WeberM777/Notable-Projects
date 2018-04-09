bba7925e433b9abc3f3f3b2d6239e202d0efc3e8
WeberM777 <marthacanizales@mail.weber.edu>
2018-04-08 19:43:12 -0600

    Added new UI Images - Added Settings Options
    Added new UI Images, Using Settings button instead of menu button, Added animator for settings button

---
+++ Assets/Scripts/SettingsPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsPanel : MonoBehaviour {
+
+	public Animator settingsAnimator;
+	bool animateSettings = false;
+
+	//Will animate settings panel
+	public void showSettingsPanel()
+	{
+		// add code for if a profile is not selected
+		animateSettings = !animateSettings;
+		settingsAnimator.SetBool("AnimateSettings", animateSettings);
+
+	}
+}
