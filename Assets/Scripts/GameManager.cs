7e4060f37d7309df2c03cb0919311df1ef5a6999
Justus Brown <justusbrown@mail.weber.edu>
2018-07-01 21:28:44 -0600

    Fixed project setup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,9 +11,11 @@ public class GameManager : MonoBehaviour {
 	const int INTERMEDIATE = 1;
 
     // Level Buttons
+    public GameObject Level1Lock;
     public GameObject Level2Lock;
     public GameObject Level3Lock;
     public GameObject Level4Lock;
+    public GameObject Level1Button;
     public GameObject Level2Button;
     public GameObject Level3Button;
     public GameObject Level4Button;
@@ -58,17 +60,23 @@ public class GameManager : MonoBehaviour {
 
             if (progress > 15)
             {
-                Level2Lock.SetActive(false);
-                Level2Button.GetComponent<Button>().interactable = true;
-                Level3Lock.SetActive(false);
-                Level3Button.GetComponent<Button>().interactable = true;
+                Level1Button.GetComponent<Button>().interactable = false;
+                Level1Lock.SetActive(true);
+                Level2Lock.SetActive(true);
+                Level2Button.GetComponent<Button>().interactable = false;
+                Level3Lock.SetActive(true);
+                Level3Button.GetComponent<Button>().interactable = false;
                 Level4Lock.SetActive(false);
                 Level4Button.GetComponent<Button>().interactable = true;
             }
             else if (progress > 10)
             {
-                Level2Lock.SetActive(false);
-                Level2Button.GetComponent<Button>().interactable = true;
+                Level1Lock.SetActive(true);
+                Level2Button.GetComponent<Button>().interactable = false;
+                Level2Lock.SetActive(true);
+                Level2Button.GetComponent<Button>().interactable = false;
+                Level4Lock.SetActive(true);
+                Level4Button.GetComponent<Button>().interactable = false;
                 Level3Lock.SetActive(false);
                 Level3Button.GetComponent<Button>().interactable = true;
             }
@@ -76,9 +84,17 @@ public class GameManager : MonoBehaviour {
             {
                 Level2Lock.SetActive(false);
                 Level2Button.GetComponent<Button>().interactable = true;
+                Level1Lock.SetActive(true);
+                Level1Button.GetComponent<Button>().interactable = false;
+                Level3Lock.SetActive(true);
+                Level3Button.GetComponent<Button>().interactable = false;
+                Level4Lock.SetActive(true);
+                Level4Button.GetComponent<Button>().interactable = false;
             }
             else
             {
+                Level1Button.GetComponent<Button>().interactable = true;
+                Level2Lock.SetActive(true);
                 Level2Lock.SetActive(true);
                 Level2Button.GetComponent<Button>().interactable = false;
                 Level3Lock.SetActive(true);
